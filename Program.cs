3bf0a3fb6a06a17a0216b92436ac2e6984e0842e
LAB-00\lab00-00 <e107ti@cps.sp.gov.br>
2022-06-22 21:29:24 -0300

    Atividade Realizada

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int resultado = 1;
+int numeroEscolhido;
+
+Console.Clear();
+Console.WriteLine("----- Fatorial -----");
+
+Console.Write("Número: ");
+numeroEscolhido = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= numeroEscolhido; i++)
+{
+    resultado *= i;
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"\nO fatorial de {numeroEscolhido} é igual a {resultado}\n");
+Console.ResetColor();
